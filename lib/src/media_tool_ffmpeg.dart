e093671b89468501db3172513ce627dce9f9ca0b
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-10-30 18:59:46 +0300

    Update media_tool_ffmpeg.dart

--- lib/src/media_tool_ffmpeg.dart
+++ lib/src/media_tool_ffmpeg.dart
@@ -1,5 +1,11 @@
+// import 'dart:io';
+import 'dart:ui';
 import 'package:media_tool_platform_interface/media_tool_platform_interface.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit.dart';
+// import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit.dart';
+// import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_session.dart';
+// import 'package:ffmpeg_kit_flutter_full_gpl/ffprobe_kit.dart';
+// import 'package:ffmpeg_kit_flutter_full_gpl/ffprobe_session.dart';
+// import 'package:ffmpeg_kit_flutter_full_gpl/return_code.dart';
 
 /// The FFmpeg implementation of [MediaToolPlatform].
 class MediaToolFFmpeg extends MediaToolPlatform {
@@ -8,6 +14,15 @@ class MediaToolFFmpeg extends MediaToolPlatform {
     MediaToolPlatform.instance = MediaToolFFmpeg();
   }
 
+  /// Compress video file
+  /// [id] - Unique process ID
+  /// [path] - Path location of input video file
+  /// [destination] - Path location of output video file
+  /// [videoSettings] - Video settings: codec, bitrate, quality, resolution
+  /// [skipAudio] - If `true` then audio is skipped
+  /// [audioSettings] - Audio settings: codec, bitrate, sampleRate
+  /// [overwrite] - Should overwrite exisiting file at destination
+  /// [deleteOrigin] - Should input video file be deleted on succeed compression
   @override
   Stream<CompressionEvent> startVideoCompression({
     required String id,
@@ -23,6 +38,13 @@ class MediaToolFFmpeg extends MediaToolPlatform {
     throw UnimplementedError();
   }
 
+  /// Compress audio file
+  /// [id] - Unique process ID
+  /// [path] - Path location of input video file
+  /// [destination] - Path location of output video file
+  /// [settings] - Audio settings: codec, bitrate, sampleRate
+  /// [overwrite] - Should overwrite exisiting file at destination
+  /// [deleteOrigin] - Should input audio file be deleted on succeed compression
   @override
   Stream<CompressionEvent> startAudioCompression({
     required String id,
@@ -36,12 +58,19 @@ class MediaToolFFmpeg extends MediaToolPlatform {
     throw UnimplementedError();
   }
 
+  /// Cancel current compression process
   @override
   Future<bool> cancelCompression(String id) {
     // TODO: implement cancelCompression
     throw UnimplementedError();
   }
 
+  /// Convert image file
+  /// [path] - Path location of input video file
+  /// [destination] - Path location of output video file
+  /// [settings] - Image settings: format, quality, size
+  /// [overwrite] - Should overwrite exisiting file at destination
+  /// [deleteOrigin] - Should input image file be deleted on succeed compression
   @override
   Future<ImageInfo?> imageCompression({
     required String path,
@@ -54,6 +83,13 @@ class MediaToolFFmpeg extends MediaToolPlatform {
     throw UnimplementedError();
   }
 
+  /// Extract thumbnails from video file
+  /// [path] - Path location of input video file
+  /// [requests] - Time points of thumbnails including destination path for each
+  /// [settings] - Image settings: format, quality, size
+  /// [transfrom] - A flag to apply preferred source video tranformations to thumbnail
+  /// [timeToleranceBefore] - Time tolerance before specified time, in seconds
+  /// [timeToleranceAfter] - Time tolerance after specified time, in seconds
   @override
   Future<List<VideoThumbnail>> videoThumbnails({
     required String path,
