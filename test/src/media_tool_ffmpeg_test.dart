cc8fd6baf2d7a196ec1ff632b4fd951ffc21cebc
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-11-22 18:48:13 +0300

    Update media_tool_ffmpeg_test.dart

--- test/src/media_tool_ffmpeg_test.dart
+++ test/src/media_tool_ffmpeg_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 import 'package:media_tool_ffmpeg/media_tool_ffmpeg.dart';
 
 void main() {
-  group('MediaToolFfmpeg', () {
+  group('MediaToolFFmpeg', () {
     test('can be instantiated', () {
       expect(MediaToolFFmpeg(), isNotNull);
     });
